146c5daafdcc4dcba3382cd81dbffc5925124ca6
ny0011 <nayun011@gmail.com>
2019-07-26 17:36:09 +0900

    #3.2 Making a JS Clock part Two

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # js-basics
- Vanila JS
+
+Vanila JS
+
+#### 1. 시계를 만들자
+
+-   XX:XX:XX AM/PM 으로 표시되는 시계
+-   1초마다 갱신됨
+-   ``(backtick) 을 사용하면 변수와 문자열을 쉽게 표기할 수 있음
+    https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Template_literals
+-   함수를 매 초 call할 때 성능이 안좋진 않을까?
+-   몇 자리수를 string으로 반환하는 함수 : str.padStart()
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
--- clock.js
+++ clock.js
@@ -1,26 +1,45 @@
 const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+	clockTitle = clockContainer.querySelector("h1");
+
+/*
+function changeTimeFormat(strings, time) {
+	return time < 10 ? `0${time}` : time;
+}
+*/
 
 function getTime() {
-  const date = new Date();
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
-  const seconds = date.getSeconds();
-  let timeString = `${hours}`;
-  let time12hour = `AM`;
+	const date = new Date();
+	const minutes = date.getMinutes();
+	const hours = date.getHours();
+	const seconds = date.getSeconds();
+	let hourInt = `${hours}`;
+	let time12hour = "AM";
 
-  if (hours > 12) {
-    timeString = `${hours - 12}`;
-    time12hour = `PM`;
-  } else if (hours === 0) {
-    timeString = 12;
-  }
+	if (hours >= 12) {
+		hourInt = `${hours - 12}`;
+		time12hour = "PM";
+	}
 
-  clockTitle.innerHTML = `${timeString}:${minutes}:${seconds} ${time12hour}`;
+	if (hours === 0 || hours === 12) {
+		hourInt = 12;
+	}
+
+	clockTitle.innerHTML = `${hourInt
+		.toString()
+		.padStart(2, "0")}:${minutes
+		.toString()
+		.padStart(2, "0")}:${seconds
+		.toString()
+		.padStart(2, "0")} ${time12hour}`;
+	//`${changeTimeFormat`${hourInt}`}:${changeTimeFormat`${minutes}`}:${changeTimeFormat`${seconds}`} ${time12hour}`;
+	/*  `${hourInt < 10 ? `0${hourInt}` : hourInt}:${
+		minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds} ${time12hour}`;
+  */
 }
 
 function init() {
-  getTime();
+	setInterval(getTime, 1000);
 }
 
 init();
